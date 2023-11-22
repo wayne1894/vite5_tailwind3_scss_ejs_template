b0aaf7e7fb265a3260cfc5c000528df0b06f135c
wayne1894 <mose286778@gmail.com>
2023-11-22 17:00:08 +0800

    no message

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  content: [
-    "./**/*.{html,js}", // 所有資料夾下的 HTML 和 JavaScript 文件
-    "!./node_modules/**/*", // 排除 node_modules 下的所有文件
-    "!./dist/**/*", // 排除 dist 下的所有文件
-  ],
+  content: ['./src/**/*.{html,js}'],
   theme: {},
   plugins: [],
 };
